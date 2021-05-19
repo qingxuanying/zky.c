af189bd2578eb58fee39518e13005670ec9077de
qingxiangying <597707165@qq.com>
2021-05-19 16:36:41 +0800

    5.19

--- c/c语言实验课/实验十一.c
+++ c/c语言实验课/实验十一.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 void main() 
 {
     int a,b,tmp,tag,k;
-    printf("输入区间下限a:");
+    printf("输入正整数区间下限a:");
     scanf("%d",&a);
-    printf("输入区间上限b:");
+    printf("输入正整数区间上限b:");
     scanf("%d",&b);
     k=0;
     putchar('\n');
@@ -13,11 +14,19 @@ void main()
         a=b;
         b=tmp;
     }
+    if(a<0){
+        printf("请输入正整数！");
+        exit(-1);
+    }
+
     printf("a=%d\n",a);
     printf("b=%d\n",b);
     
     for(int i=a;i<=b;i++){
         tag=1;
+        if(i<2){
+            i=2;
+        }
         for(int j=2;j<i;j++){
             if(i%j==0){
                 tag=0;
