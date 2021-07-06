782c61c2fd340c41b2088e807f7cb5ee086e7600
qingxiangying <597707165@qq.com>
2021-07-06 14:56:28 +0800

    7.6

--- c/c实训/7.5/2.c
+++ c/c实训/7.5/2.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int s[15],flag=0,j=0,del=0,n=0;
+    int s[15]={0},flag=0,j=0,del=0,n=0;
     for(int i=1;flag<15;i++)
     {
         n++;
         for(int k=0;k<j;k++){
-            if(s[k]==i){
+            if(s[k]==n){
                 del=1;
                 break;
             }
         }
-        if(del){
+        if(del == 1){
             i--;
             continue;
         }
